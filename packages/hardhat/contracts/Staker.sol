c8be7001e9bbcb89e1d5ebb425d348b899466936
kyrers <kyrers.eth@gmail.com>
2022-04-04 19:43:39 +0100

    Staker contract updates
    - Staker contract is now Ownable;
    - Stake function;
    - Threshold (minimum staked amount) can be dynamically updated;

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,24 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.13;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ExampleExternalContract.sol";
 
-contract Staker {
-
+/**
+* @title Staker contract
+* @author kyrers
+* @notice Contract for ETH staking. Mostly based on speedrunethereum challenge 1, but allows for the minimumStakedAmount to be updated under some conditions.
+ */
+contract Staker is Ownable {
+  //An example external contract to hold the staked funds, built by scaffold-eth
   ExampleExternalContract public exampleExternalContract;
 
-  constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+  //User's staked funds
+  mapping (address => uint256) public balances;
+
+  //Minimum staked amount needed
+  uint256 public minimumStakedAmount;
+
+  //Events
+  event Stake(address staker, uint256 amount);
+  //------
+  
+  //Modifiers
+  modifier isValidAmount(uint256 value) {
+    require(value < 33, "Invalid minimum staked amount.");
+    _;
+  }
+  //------
+
+   /**
+  * @notice Contract Constructor
+  * @param _exampleExternalContractAddress Address of the external contract that will hold stacked funds
+  * @param _minimumStakedAmount The minimum amount of staked ETH required for the contract to move the funds
+  */
+  constructor(address _exampleExternalContractAddress, uint256 _minimumStakedAmount) isValidAmount(_minimumStakedAmount) {
+    exampleExternalContract = ExampleExternalContract(_exampleExternalContractAddress);
+    minimumStakedAmount = _minimumStakedAmount;
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  /**
+  * @notice Function that allows users to stake ETH
+  */
+  function stake() public payable {
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
-
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
+  /**
+  * @notice Function to allow the owner of this contract to update the _minimumStakedAmount
+  * @param _minimumStakedAmount The new minimum staked amount
+  */
+  function updateMinimumStakedAmount(uint256 _minimumStakedAmount) external onlyOwner isValidAmount(_minimumStakedAmount) {
+    minimumStakedAmount = _minimumStakedAmount;
+  }
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
